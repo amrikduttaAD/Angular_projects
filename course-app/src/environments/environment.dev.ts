8d1b443c222f0968c9dab0e772262a53134a8665
Amrik Dutta <53964677+amrikduttaAD@users.noreply.github.com>
2026-02-08 15:18:46 +0530

    Update environment.dev.ts

--- course-app/src/environments/environment.dev.ts
+++ course-app/src/environments/environment.dev.ts
@@ -3,12 +3,12 @@ export const environment = {
     name: 'dev',
     firebase:{
         config:{
-            apiKey: 'AIzaSyBSHh8xcWitHLW_SATsqqZ0M-H5lxjYMeQ',
+            apiKey: '',
             authDomain: 'courseapp-1ccac.firebaseapp.com',
             projectId: 'courseapp-1ccac',
             storageBucket: 'courseapp-1ccac.appspot.com',
             messagingSenderId: '148031636269',
-            appId: '1:148031636269:web:460d5f846247931c7489a4'
+            appId: ''
         }
     }
   };
