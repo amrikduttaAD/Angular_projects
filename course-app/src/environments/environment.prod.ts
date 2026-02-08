2e70e90369d010c5a8d47ac0e24de5ce8c046636
Amrik Dutta <53964677+amrikduttaAD@users.noreply.github.com>
2026-02-08 15:17:59 +0530

    Update environment.prod.ts

--- course-app/src/environments/environment.prod.ts
+++ course-app/src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
         projectId: 'courseapp-1ccac',
         storageBucket: 'courseapp-1ccac.appspot.com',
         messagingSenderId: '148031636269',
-        appId: '1:148031636269:web:460d5f846247931c7489a4'
+        appId: ''
     }
 }
 };
